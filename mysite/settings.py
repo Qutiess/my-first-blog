9dc79d59ce46e7e5ce3073f71ebc946f4ae5922e
Qutiess <202758080+Qutiess@users.noreply.github.com>
2025-09-30 18:48:01 +0800

    Accidentally removed import os. Added it back

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+import os
 
 SECRET_KEY = os.getenv('SECRET')
 
