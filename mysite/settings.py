822deb89ad3dfb05322b053e75b30619e4171274
Qutiess <202758080+Qutiess@users.noreply.github.com>
2025-09-30 18:50:23 +0800

    Added pythonanywhere.com to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ilikepythonsnakes.pythonanywhere.com']
 
 
 # Application definition
