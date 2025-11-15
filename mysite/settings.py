8f65f0fc2384a67ebc6664a9cec703216f85f9d6
Qutiess <202758080+Qutiess@users.noreply.github.com>
2025-11-15 20:30:28 +0800

    Changed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ilikepythonsnakes.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
